1726b7888486ecd55013a119fe9e58742f32ee42
Joe Vasquez <joevasquez344@gmail.com>
2020-10-22 14:01:52 -0700

    created project with create react app and added file for home page. Also added directory and menu item component styles. Set up absolute path for imports

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.scss";
+
+import Directory from "components/Directory";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Directory />
+    </div>
+  );
+};
+
+export default Home;
